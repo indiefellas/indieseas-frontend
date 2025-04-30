d389d17b3b37ee61bd7c0e282fadb202c6bdb4e4
Jb Carreon <jbcarreon212@gmail.com>
2025-04-30 11:53:10 +0800

    bare stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import node from '@astrojs/node';
+
+import nurodevbun from '@nurodev/astro-bun';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  adapter: node({
+    mode: 'middleware'
+  }),
+});
---
+++ entry.mjs
@@ -0,0 +1,11 @@
+import express from 'ultimate-express';
+import { handler as ssrHandler } from './dist/server/entry.mjs';
+
+const app = express();
+const base = '/';
+app.use(base, express.static('dist/client/'));
+app.use(ssrHandler);
+
+app.listen(4321);
+
+console.log(`Listening on port 4321`);
